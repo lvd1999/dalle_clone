256967b97ec1d1ece77ab763d0dcff17574f6e82
eleevoo <voon.dic.lee@ericsson.com>
2023-03-01 10:32:39 +0000

    open ai api setup

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -4,6 +4,8 @@ import { v2 as cloudinary } from "cloudinary";
 
 import Post from "../mongodb/models/post.js";
 
-dotenv.config(); // Load environment variables from .env file, where API keys and passwords are configured
+dotenv.config(); // Load environment variables from .env file
 
 const router = express.Router();
+
+export default router;
